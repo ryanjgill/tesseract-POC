8f69e95df5a86feb111b114f02d2e435cf6fb8f9
Ryan Gill <ryan.j.gill@nasa.gov>
2015-04-20 10:41:45 -0500

    merged other files`

--- app.js
+++ app.js
@@ -6,11 +6,32 @@ var path = require('path');
 var async = require('async');
 var util = require('util');
 var pdfText = require('pdf-text');
+var multer = require('multer');
+var rimraf = require('rimraf');
+var mkdirp = require('mkdirp');
+var done = false;
 
 var app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(__dirname + '/index.html'));
+
+/*Configure the multer.*/
+app.use(
+	multer({
+		dest: './uploads/',
+		rename: function (fieldname, filename) {
+		  return filename.replace(/ /g, '_');
+		},
+		onFileUploadStart: function (file) {
+
+		},
+		onFileUploadComplete: function (file) {
+		  console.log(file.originalname.cyan + ' uploaded to  ' + file.path.yellow);
+		  done=true;
+		}
+	})
+);
 
 var manifest = [];
 
@@ -18,15 +39,20 @@ var server = app.listen(3000, function () {
 
   var host = server.address().address;
   var port = server.address().port;
-  
-
 
 	app.get('/', function (req, res) {
 	  res.sendFile(path.join(__dirname + '/public/index.html'));
 	});
 
+	app.post('/uploads',function(req,res){
+	  if(done==true){
+	    console.log(req.files);
+	    res.sendFile(path.join(__dirname + '/public/results.html'));
+	  }
+	});
+
 	app.get('/start', function (req, res) {
-		var folder = 'images';
+		var folder = 'uploads';
 	  var supportedTypes = [".JPG", ".jpg", ".tif"];
 
 	  manifest = [];
@@ -38,12 +64,12 @@ var server = app.listen(3000, function () {
 	  // loop over directory and get all the images
 		fs.readdir(__dirname + '\\' + folder + '\\', function(err, files) {
 	    console.log('List of files: ');
-	    files.forEach(function(file) { 
+	    files.forEach(function(file) {
 	    	console.log(file);
 	    });
 
 
-	    var temp = files.filter(function (file) { 
+	    var temp = files.filter(function (file) {
 				return supportedTypes.indexOf(path.extname(file)) >= 0;
 			});
 
@@ -51,15 +77,16 @@ var server = app.listen(3000, function () {
 				return ['.pdf', '.PDF'].indexOf(path.extname(file)) >= 0;
 			});
 
-			console.log('List of PDFs: '.red.bold);
-			pdfs.forEach(function(file) { 
+			console.log(colors.cyan.bold('%s of %s files are PDFs!'), pdfs.length, files.length);
+			console.log('List of PDFs: '.yellow.bold);
+			pdfs.forEach(function(file) {
 	    	console.log(file.red);
 	    });
 
-			console.log(colors.cyan.bold('%s of %s files are images!'), temp.length, files.length);
+			console.log(colors.cyan.bold('%s of %s files are Images!'), temp.length, files.length);
 			console.log('List of images: '.yellow.bold);
-			
-			temp.forEach(function(file) { 
+
+			temp.forEach(function(file) {
 	    	console.log(file.cyan);
 	    });
 
@@ -69,7 +96,7 @@ var server = app.listen(3000, function () {
 	    	function (cb) {
 	    		console.log(colors.bold.red('************ starting the OCR ***************'));
 			    async.each(temp, function (file, done) {
-			    	console.log(file);
+			    	console.log(file.yellow.bold);
 			    	processOCR(folder, file, done);
 			    	//done(); // I fucked something up :(
 			    }, function (err) {
@@ -95,6 +122,12 @@ var server = app.listen(3000, function () {
 	    	}
 	    ], function (err, results) {
 	    	console.log("Manifest updated successfully!".green);
+	    	rimraf(__dirname + '/uploads', function () {
+	    		console.log('removed uploads directory'.red);
+	    		mkdirp(__dirname + '/uploads', function () {
+	    			console.log('created uploads directory'.green);
+	    		});
+	    	});
 	    	res.send(manifest);
 	    }); 
 	    
